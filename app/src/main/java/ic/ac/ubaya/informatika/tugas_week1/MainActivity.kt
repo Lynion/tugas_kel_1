acd20c75e8476e0de9f6951443df308e9a474631
Dylan Sherwood <dylansherwood35@gmail.com>
2021-08-12 15:24:40 +0700

    initial commit

---
+++ app/src/main/java/ic/ac/ubaya/informatika/tugas_week1/MainActivity.kt
@@ -0,0 +1,11 @@
+package ic.ac.ubaya.informatika.tugas_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
