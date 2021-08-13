7e158c6d454e8ff4759e4619c8967264c0ccd964
alungseptiann <s160418055@student.ubaya.ac.id>
2021-08-13 12:22:01 +0700

    phone and invitation acc

--- app/src/main/java/ic/ac/ubaya/informatika/tugas_week1/MainActivity.kt
+++ app/src/main/java/ic/ac/ubaya/informatika/tugas_week1/MainActivity.kt
@@ -2,11 +2,16 @@ package ic.ac.ubaya.informatika.tugas_week1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private var name:String = "dfgh"
+    private var name:String = "Septian"
+    private var phone:String = "12345"
+    private var acc:String = "Invitation Acc"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    Toast.makeText(this,"Wellcome To Android $name", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,"$acc with phone number $phone", Toast.LENGTH_SHORT).show()
     }
 }
