db9d1414917045dd4c304f86a01861cd596f23b7
Dylan Sherwood <dylansherwood35@gmail.com>
2021-08-13 12:43:08 +0700

    Dylan's Branch

--- app/src/main/java/ic/ac/ubaya/informatika/tugas_week1/MainActivity.kt
+++ app/src/main/java/ic/ac/ubaya/informatika/tugas_week1/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    private var name:String = "sdfg"
+    private var name1:String = "Dylan"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
