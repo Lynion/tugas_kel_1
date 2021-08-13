db901d29143c3f288307a0df4a2dbc288aae15cd
alungseptiann <s160418055@student.ubaya.ac.id>
2021-08-13 12:38:03 +0700

    New Page and Try Intent

--- app/src/main/java/ic/ac/ubaya/informatika/tugas_week1/MainActivity.kt
+++ app/src/main/java/ic/ac/ubaya/informatika/tugas_week1/MainActivity.kt
@@ -1,8 +1,10 @@
 package ic.ac.ubaya.informatika.tugas_week1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var name:String = "Septian"
@@ -13,5 +15,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     Toast.makeText(this,"Wellcome To Android $name", Toast.LENGTH_SHORT).show()
         Toast.makeText(this,"$acc with phone number $phone", Toast.LENGTH_SHORT).show()
+
+        txtSalam.text = "Wellcome $name"
+        btnNext.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/ic/ac/ubaya/informatika/tugas_week1/SecondActivity.kt
@@ -0,0 +1,11 @@
+package ic.ac.ubaya.informatika.tugas_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
